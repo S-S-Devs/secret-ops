13d8a858d970321e60df2ca9db3bc3f3fe25a5b6
Monster User <monsteruser@gmail.com>
2025-02-05 18:34:35 -0400

    Mega correciones ;-;

--- main.js
+++ main.js
@@ -1,11 +1,24 @@
-const signInBtnLink = document.querySelector('.signInBtn-link');
-const signUpBtnLink = document.querySelector('.signUpBtn-link');
-const wrapper = document.querySelector('.wrapper');
+document.getElementById('current-date').textContent = 'Fecha actual: ' + new Date().toLocaleDateString();
+document.getElementById('last-modified').textContent = 'Última modificación: ' + document.lastModified;
 
-signUpBtnLink.addEventListener('click', () => {
-  wrapper.classList.toggle('active');
-});
-
-signInBtnLink.addEventListener('click', () => {
-  wrapper.classList.toggle('active');
-});
+const browserInfo = navigator.userAgent;
+let browserName, fullVersion;
+if ((verOffset = browserInfo.indexOf("Chrome")) != -1) {
+    browserName = "Chrome";
+    fullVersion = browserInfo.substring(verOffset + 7);
+} else if ((verOffset = browserInfo.indexOf("Firefox")) != -1) {
+    browserName = "Firefox";
+    fullVersion = browserInfo.substring(verOffset + 8);
+} else if ((verOffset = browserInfo.indexOf("MSIE")) != -1) {
+    browserName = "Internet Explorer";
+    fullVersion = browserInfo.substring(verOffset + 5);
+} else if ((verOffset = browserInfo.indexOf("Safari")) != -1) {
+    browserName = "Safari";
+    fullVersion = browserInfo.substring(verOffset + 7);
+    if ((verOffset = browserInfo.indexOf("Version")) != -1)
+        fullVersion = browserInfo.substring(verOffset + 8);
+} else {
+    browserName = "Unknown";
+    fullVersion = "Unknown";
+}
+document.getElementById('browser-info').textContent = `Navegador: ${browserName}, Versión: ${fullVersion.split(' ')[0]}`;
